7421f5099c818c3735099393302f097fb55e50c3
fturdean <fturdean@sfu.ca>
2022-04-24 01:31:24 -0700

    add Security class and tests

--- capital_gains.py
+++ capital_gains.py
@@ -1,13 +1,15 @@
+from functools import total_ordering
 from nltk.tokenize import word_tokenize
 from datetime import datetime
 
 class Trade:
-    def __init__(self, date, security, quantity, price):
+    def __init__(self, date, security, quantity, price, fee):
         # date must be in ISO 8601 format: YYYY-mm-dd
         self.date = datetime.strptime(date, "%Y-%m-%d").date()
         self.security = security
         self.quantity = quantity
         self.price = price
+        self.fee = fee
 
     def __str__(self):
         return f'{self.date} {self.security} {self.quantity} ${self.price}'
@@ -23,3 +25,41 @@ class Trade:
 
     def get_price(self):
         return self.price
+
+@total_ordering
+class Security:
+    def __init__(self, id):
+        self.id = id
+        self.trades = []
+        self.acb = 0
+        self.balance = 0
+        self.acb_per_share = 0
+
+    def __str__(self):
+        return f'''{self.id}\n Number Trades: {len(self.trades)} Remaining Balance: {self.balance}
+        ACB: {self.acb}'''
+
+    def __eq__(self, other):
+        return self.id == other.id
+    
+    def __lt__(self, other):
+        return self.id < other.id
+
+    def update_acb(self, trade):
+        assert(trade.quantity != 0)
+        # opening transaction
+        if trade.quantity > 0:
+            self.acb += trade.price + trade.fee
+            self.balance += trade.quantity
+            self.acb_per_share = self.acb / self.balance
+        # closing transaction
+        else:
+            self.acb -= self.acb_per_share * trade.quantity
+            self.balance += trade.quantity
+            if self.balance == 0:
+                self.acb = 0
+                self.acb_per_share = 0
+        
+    def add_trade(self, trade):
+        self.trades.append(trade)
+        self.update_acb(trade)
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 from datetime import datetime
-from capital_gains import Trade
+from capital_gains import Trade, Security
 
 class TestTrade(unittest.TestCase):
     def setUp(self):
@@ -8,7 +8,8 @@ class TestTrade(unittest.TestCase):
         self.security = 'NAME'
         self.quant = 10
         self.price = 150.99
-        self.trade = Trade(self.today, self.security, self.quant, self.price)
+        self.fee = 10.99
+        self.trade = Trade(self.today, self.security, self.quant, self.price, self.fee)
 
     # test Trade object instantiation
     def test_trade_create(self):
@@ -30,6 +31,39 @@ class TestTrade(unittest.TestCase):
     #test price assignment
     def test_price_assign(self):
         self.assertEqual(self.price, self.trade.price)
+
+    #test price assignment
+    def test_price_assign(self):
+        self.assertEqual(self.fee, self.trade.fee)
+
+class TestSecurity(unittest.TestCase):
+    def setUp(self):
+        self.today = datetime.now().strftime("%Y-%m-%d")
+        self.trade1 = Trade(self.today, 'AAPL', 10, 150.99, 10.99)
+        self.trade2 = Trade(self.today, 'AAPL', -10, 120.25, 10.99)
+        self.security = Security('AAPL')
+
+    def test_security_create(self):
+        self.assertIsNotNone(self.security)
+        self.assertEqual(self.security.id, 'AAPL')
+
+    def test_add_trades(self):
+        self.security.add_trade(self.trade1)
+        self.assertEqual(len(self.security.trades), 1)
+        self.assertEqual(self.security.acb, self.trade1.price+self.trade1.fee) 
+        self.assertEqual(self.security.balance, self.trade1.quantity)
+        self.assertEqual(self.security.acb_per_share, self.security.acb/self.security.balance)
+
+        self.security.add_trade(self.trade2)
+        self.assertEqual(len(self.security.trades), 2)
+        self.assertEqual(self.security.acb, 0)
+        self.assertEqual(self.security.balance, 0)
+        self.assertEqual(self.security.acb_per_share, 0)
+
+    def test_ordering(self):
+        security2 = Security("AMD")
+        self.assertLess(self.security, security2)
+        self.assertNotEqual(security2, self.security)
 
 if __name__ == '__main__':
     unittest.main()
